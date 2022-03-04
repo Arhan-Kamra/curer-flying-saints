f7260f7db5fb0cddf61f4f156215ff6a071a1b0f
Arhan Kamra <arhankamra@yahoo.com>
2022-03-04 16:20:30 +0530

    Add all files & initialize repository

---
+++ js/script.js
@@ -0,0 +1,199 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active2");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+const imageA = document.querySelector("#a");
+const imageB = document.querySelector("#b");
+const imageC = document.querySelector("#c");
+const imageD = document.querySelector("#d");
+const imageE = document.querySelector("#e");
+const imageF = document.querySelector("#f");
+const imageG = document.querySelector("#g");
+const imageH = document.querySelector("#h");
+const imageI = document.querySelector("#i");
+const imageJ = document.querySelector("#j");
+const imageK = document.querySelector("#k");
+const imageL = document.querySelector("#l");
+
+imageA.addEventListener(
+  "mouseenter",
+  () => {
+    imageA.src = "./images/aaa.png";
+  },
+  false
+);
+
+imageB.addEventListener(
+  "mouseenter",
+  () => {
+    imageB.src = "./images/aaa.png";
+  },
+  false
+);
+imageC.addEventListener(
+  "mouseenter",
+  () => {
+    imageC.src = "./images/aaa.png";
+  },
+  false
+);
+imageD.addEventListener(
+  "mouseenter",
+  () => {
+    imageD.src = "./images/aaa.png";
+  },
+  false
+);
+imageE.addEventListener(
+  "mouseenter",
+  () => {
+    imageE.src = "./images/aaa.png";
+  },
+  false
+);
+imageF.addEventListener(
+  "mouseenter",
+  () => {
+    imageF.src = "./images/aaa.png";
+  },
+  false
+);
+imageG.addEventListener(
+  "mouseenter",
+  () => {
+    imageG.src = "./images/aaa.png";
+  },
+  false
+);
+imageH.addEventListener(
+  "mouseenter",
+  () => {
+    imageH.src = "./images/aaa.png";
+  },
+  false
+);
+imageI.addEventListener(
+  "mouseenter",
+  () => {
+    imageI.src = "./images/aaa.png";
+  },
+  false
+);
+imageJ.addEventListener(
+  "mouseenter",
+  () => {
+    imageJ.src = "./images/aaa.png";
+  },
+  false
+);
+imageK.addEventListener(
+  "mouseenter",
+  () => {
+    imageK.src = "./images/aaa.png";
+  },
+  false
+);
+imageL.addEventListener(
+  "mouseenter",
+  () => {
+    imageL.src = "./images/aaa.png";
+  },
+  false
+);
+
+imageA.addEventListener(
+  "mouseleave",
+  () => {
+    imageA.src = "./images/Group 46578.png";
+  },
+  false
+);
+
+imageB.addEventListener(
+  "mouseleave",
+  () => {
+    imageB.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageC.addEventListener(
+  "mouseleave",
+  () => {
+    imageC.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageD.addEventListener(
+  "mouseleave",
+  () => {
+    imageD.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageE.addEventListener(
+  "mouseleave",
+  () => {
+    imageE.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageF.addEventListener(
+  "mouseleave",
+  () => {
+    imageF.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageG.addEventListener(
+  "mouseleave",
+  () => {
+    imageG.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageH.addEventListener(
+  "mouseleave",
+  () => {
+    imageH.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageI.addEventListener(
+  "mouseleave",
+  () => {
+    imageI.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageJ.addEventListener(
+  "mouseleave",
+  () => {
+    imageJ.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageK.addEventListener(
+  "mouseleave",
+  () => {
+    imageK.src = "./images/Group 46578.png";
+  },
+  false
+);
+imageL.addEventListener(
+  "mouseleave",
+  () => {
+    imageL.src = "./images/Group 46578.png";
+  },
+  false
+);
